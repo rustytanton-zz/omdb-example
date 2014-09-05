027096b6c8e73c20c09ce344ebd41726b814f327
Rusty Tanton <rtanton@webmd.net>
2014-09-05 00:58:55 -0400

    movie results page working, simplified search form

--- app.js
+++ app.js
@@ -11,30 +11,34 @@ angular.module("OmdbSearch", ["ngRoute"])
 
 	.controller("HomeController", function ($scope, $location) {
 		$scope.search = function() {
-			$location.url(
-				"/search/title/" + $scope.t
-				+ "/year/" + $scope.y
-				+ "/plot/" + $scope.plot
-			);
+			$location.url("/search/" + $scope.t);
 		};
 	})
 
-	.controller("MovieController", function ($scope, $routeParams) {
-		
+	.controller("MovieController", function ($scope, $routeParams, OmdbApi) {
+		OmdbApi.search($routeParams).then(function(result) {
+			$scope.done = true;
+
+			$scope.title = result.data.Title;
+			delete result.data.Title;
+			
+			$scope.poster = result.data.Poster;
+			delete result.data.Poster;
+
+			delete result.data.imdbID;
+			delete result.data.Type;
+
+			$scope.movie = result.data;
+		}, function() {
+			$scope.error = true;
+		});		
 	})
 
 	.controller("SearchController", function ($scope, $routeParams, OmdbApi) {
 		$scope.searching = true;
-		angular.forEach($routeParams, function(value, key) {
-			if (!value || value == "undefined") {
-				delete $routeParams[key];
-			}
-		});
-		$scope.showYouSearchedFor = $routeParams.t || $routeParams.y || $routeParams.plot;
 		$scope.$routeParams = $routeParams;
 		OmdbApi.search($routeParams).then(function(result) {
 			$scope.searching = false;
-			console.log(result);
 			if (result.data.Error) {
 				$scope.error = true;
 			} else {
@@ -49,11 +53,11 @@ angular.module("OmdbSearch", ["ngRoute"])
 
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
-			.when("/movie/:id", {
+			.when("/movie/:i", {
 				templateUrl: '/partials/movie.html',
 				controller: 'MovieController',
 			})
-			.when("/search/title/:s?/year/:y?/plot/:plot?", {
+			.when("/search/:s", {
 				templateUrl: '/partials/search.html',
 				controller: 'SearchController',
 			})
